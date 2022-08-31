d0ecf38469038e54660aa53127972d8c69fdd729
pcheung <pika206@gmail.com>
2022-08-30 20:55:20 -0500

    fix reset button to clear li-s

--- js/app.js
+++ js/app.js
@@ -1,55 +1,45 @@
-
-//* DOM selectors
+//* Initial DOM selectors and event listeners
 input_DOM = document.querySelector('#task-input')
 submitBtn_DOM = document.querySelector('#submit-button')
 resetBtn_DOM = document.querySelector('#reset-button')
-todoListContainer_DOM = document.querySelector('#todo-list--container')
+todoListContainer_DOM = document.querySelector('#todo-list--container') //wrapper for dynamically created list
 todoList_DOM = document.querySelector('#todo-list')
 
-
-////san checks
-console.dir(input_DOM)
-console.dir(submitBtn_DOM)
-console.dir(todoList_DOM)
-console.dir(todoListContainer_DOM)
-console.dir(resetBtn_DOM)
-
-//* event listeners
 submitBtn_DOM.addEventListener('click', newTask)
 resetBtn_DOM.addEventListener('click', reset)
 todoList_DOM.addEventListener('click', deleteTask)
 
-//* Main functions
+//* action functions
 function newTask(){
-  //// create new li 
-  //// set newTask text to value of input
-  //// add newTask to the todoList
-  //// clear the value in the input
+  /* create new li, set newTask text to value of input, add newTask to the todoList, clear the value in the input */
   if (input_DOM.value) {
     let newTask_DOM = document.createElement('li')
     newTask_DOM.innerText = input_DOM.value
     newTask_DOM.classList.add('listItem')
-    console.log(newTask_DOM) //san check
+    console.log(newTask_DOM) //!san check when adding a new task
     todoList_DOM.append(newTask_DOM)
     resetInput()
   }
 }
 
 function reset(){
-  ////clears any value in the input and reinit the list
+  //clears any value in the input and removes all of the li's from the ul
   resetInput()
-  oldtasks_DOM = document.querySelectorAll('.listItem')
-  console.log(oldtasks_DOM)
-  //! here's where I left off
+  todoList_DOM.innerHTML = ''
 }
 
 function deleteTask(event){
-  ////remove the target task li that was clicked when called upon
+  //remove the target task li that was clicked when called upon
   event.target.remove()
 }
 
 //* helper functions
 function resetInput(){
-  input_DOM.value = ''
+  input_DOM.value = '' //clears any value in the input box
 }
 
+//! san checks
+// console.dir(input_DOM)
+// console.dir(submitBtn_DOM)
+// console.dir(resetBtn_DOM)
+// console.dir(todoListContainer_DOM)
